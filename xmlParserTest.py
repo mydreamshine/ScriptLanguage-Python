b90c587c59ec09e14d0edc2f67345441dcceeef6
Myeong Joon <mydreamshine@naver.com>
2018-04-05 20:25:34 +0900

    xml->dictionary parsing
    URL에서 urllib.urlopen()으로 url 전달 시 "http://"문장이 자꾸 제외되서 전달되는 현상이 일어난다...

--- xmlParserTest.py
+++ xmlParserTest.py
@@ -6,16 +6,17 @@ import urllib.parse
 import json
 
 
-def getDustinformation(sidoName):
+def getDustinformation(Address):
     url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?"
     key = "serviceKey=" + "Iq26py4%2BXq0dUHvM7eXZdaHnh3O8fDOQOTVjMw95Kh94fKJqLYh%2FZH%2BaZ7%2FD%2BojU7RJ6ERZXP8M%2BH%2BnL8a4cbQ%3D%3D"
     numOfRows = "&numOfRows=" + "1"
+    sidoname = "&sidoName=" + Address
     ver = "&ver=" + "1.3"
     type = "&_returnType=json"
 
-    api_url = url + key + numOfRows + "&" + sidoName + ver + type
+    api_url = url + key + numOfRows + sidoname + ver + type
 
-    data = urllib.request.urlopen(api_url).read().decode('utf8')
+    data = urllib.request.urlopen(api_url) # .read().decode('utf8')
     data_json = json.loads(data)
     print(data_json)
 
